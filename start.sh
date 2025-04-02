5be72b58d460b8dde04e1a6ff46b95d07d749075
Luisa Alvear <luisalvear@gmail.com>
2025-04-02 03:43:54 -0300

    Corrige puerto para compatibilidad con Render usando \$PORT

--- start.sh
+++ start.sh
@@ -4,4 +4,4 @@ echo "Generando índice vectorial..."
 python script_indexar_openai.py
 
 echo "Iniciando servidor FastAPI..."
-uvicorn main:app --host 0.0.0.0 --port 10000
+uvicorn main:app --host 0.0.0.0 --port $PORT
