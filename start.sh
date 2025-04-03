8cf2eb284e8eba542c1c5c804b038ceaf308ab4d
luchoalvear <luisalvear@gmail.com>
2025-04-03 18:14:36 -0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,8 +4,5 @@ set -e
 echo "Limpiando carpeta storage..."
 python limpiar_storage.py
 
-echo "Generando índice vectorial..."
-python script_indexar_openai.py
-
 echo "Iniciando servidor FastAPI..."
 uvicorn main:app --host 0.0.0.0 --port $PORT
